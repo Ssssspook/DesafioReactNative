24818d2e2348a2aa139d1a00af9da088aaf1ed4b
Ssssspook <carlosgustavo.rezende@estudante.ufjf.br>
2024-03-04 22:55:48 -0300

    tab bar estilizada

--- src/rotas/AppRotas.js
+++ src/rotas/AppRotas.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Home from '../../src/telas/Home';
 import LoginRotas from './LoginRotas';
@@ -12,11 +13,35 @@ const Tab = createBottomTabNavigator();
 
 export default function AppRotas(){
     return <NavigationContainer>
-    <Tab.Navigator screenOptions={{headerShown: false}} >
+    <Tab.Navigator screenOptions={({ route }) => ({
+      headerShown: false,
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'home'
+            : 'home';
+        } else if (route.name === 'Contato') {
+          iconName = focused ? 'call' : 'call';
+        } else if (route.name === 'Membros') {
+          iconName = focused ? 'people' : 'people';
+        }
+        else if (route.name === 'Login') {
+          iconName = focused ? 'log-in' : 'log-in';
+        }
+        
+        return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      tabBarActiveBackgroundColor: "#F1BE83",
+      tabBarInactiveBackgroundColor: "#F1BE83",
+      tabBarActiveTintColor: "black",
+      tabBarInactiveTintColor: "#565656",
+      })} >
       <Tab.Screen name='Home' component={Home}/>
-      <Tab.Screen name='Login' component={LoginRotas}/>
       <Tab.Screen name='Contato' component={Contato}/>
       <Tab.Screen name='Membros' component={Membros}/>
+      <Tab.Screen name='Login' component={LoginRotas}/>
     </Tab.Navigator>
   </NavigationContainer>
 }
